8860ed4d1c9ad46fe5eafb982c75f8aeb9328955
Devraj0403 <devkalkhanday123@gmail.com>
2025-01-19 19:22:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Add background and transform effects when scrolling
+window.addEventListener('scroll', function () {
+  const navbar = document.querySelector('.navbar');
+  if (window.scrollY > 50) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
